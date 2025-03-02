a4803a27a6843274c3e7d0cd5dbb9a05ec42ef9f
Ferdinand Anton <ferdinand.anton@outlook.com>
2025-03-02 21:15:06 +0700

    update

--- src/test/java/org/ogya/steps/CommonSteps.java
+++ src/test/java/org/ogya/steps/CommonSteps.java
@@ -1,18 +1,15 @@
 package org.ogya.steps;
 
 import io.cucumber.java.en.Given;
-import org.ogya.pages.LoginPage;
 import org.ogya.utils.DriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
-public class NavigationSteps {
-    WebDriver driver;
-    LoginPage loginPage;
+public class CommonSteps {
+    private final WebDriver driver;
 
-    public NavigationSteps() {
+    public CommonSteps() {
         this.driver = DriverManager.getDriver();
-        loginPage = new LoginPage(driver);
     }
 
     @Given("user is in the login page")
